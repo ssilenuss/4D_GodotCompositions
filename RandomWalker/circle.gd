829afeab5f5436f501868d354124186e97201952
JG <54590430+jls-has@users.noreply.github.com>
2026-02-03 09:53:32 -0500

    update

--- RandomWalker/circle.gd
+++ RandomWalker/circle.gd
@@ -6,3 +6,4 @@ var saturation : float = 0.0
 var value : float = 0.0
 var radius : float = 10.0
 var position : Vector2 = Vector2(0,0)
+var color : Color 
--- RandomWalker/random_walker_2d.gd
+++ RandomWalker/random_walker_2d.gd
@@ -1,36 +1,36 @@
 extends Control
 
-func _ready()->void:
+var circles : Array[Circle] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
 	RenderingServer.viewport_set_clear_mode(get_viewport().get_viewport_rid(), RenderingServer.VIEWPORT_CLEAR_NEVER)
 	
-	for i in 1000:
-		var circle = Circle.new()
-		circle.radius = 1.0
-		circle.position = Vector2(randf(), randf())*size  #size/2.0
-		circle.color = Color(0.863, randf(), 0.0, 1.0)
-		add_child(circle)
+	for i in 100:
+		var c : Circle = Circle.new()
+		c.position = Vector2(randf()*size.x, randf()*size.y)
+		c.radius = randf()*100
+		c.h = randf()
+		c.s = randf()
+		c.v = randf()
+		c.a = randf()
+		c.color = Color.from_hsv(c.h,c.s,c.v,c.a)
+		circles.append(c)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	
-func _process(_delta: float) -> void:
 	
-	for circle in get_children():
-		var x : float = randi_range(-1, 1)
-		var y : float = randi_range(-1, 1)
+	for c in circles:
+		var x : int = randi_range(-1, 1)
+		var y : int = randi_range(-1, 1)
+		c.position += Vector2(x,y)
 	
-		var g : float = randf_range(-.001,0.001)
-		var a : float = randf_range(-.001,0.001)
-		var radius : float = randf_range(-1.0,1.0)
-		circle.position += Vector2(x,y)
-		circle.position.x = wrapf(circle.position.x, 0, size.x)
-		circle.position.y = wrapf(circle.position.y, 0, size.y)
-		
-		circle.color.g += g
-		circle.color.a += a
-		
-		circle.radius += radius
-		circle.queue_redraw()
-#exercises:
-#	1. Change color over time
-#	2. change size over time
-#	3. make multiple walkers
-	
+	queue_redraw()
+
+
+func _draw() -> void:
+	for c in circles:
+		draw_circle(c.position, c.radius, c.color)
 	
