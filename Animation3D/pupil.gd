f4cab3faf4a3e52b89d083f1cc1941bc117d32bd
JLS <54590430+jls-has@users.noreply.github.com>
2026-02-10 08:25:27 -0500

    update

--- Animation3D/pupil.gd
+++ Animation3D/pupil.gd
@@ -1,12 +1,5 @@
-@tool
 extends MeshInstance3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	scale =  Vector3(0.16, 0.64, 0.58) *sin(Time.get_ticks_msec()/1000.0)
